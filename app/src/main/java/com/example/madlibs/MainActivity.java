3f6179b8a3530f6813043a270bd5ae5077c00049
Patman526 <breenboyz@gmail.com>
2019-08-29 11:16:22 -0500

    First screen done-ish

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /*
+
+    public void sendInfo(View v){
+        EditText edit = (EditText) findViewById(R.id.myName);
+        EditText myAge = (EditText) findViewById(R.id.myAge);
+
+        String myNameStr = myName.getText().toString();
+        String myAgeStr = myAge.getText().toString();
+
+        Intent intent = new Intent(this, InfoActivity.class);
+
+        intent.putExtra(InfoActivity.MY_NAME, myNameStr);
+        intent.putExtra(InfoActivity.MY_AGE, myAgeStr);
+
+        startActivity(intent);
+    }
+    */
+    
 }
