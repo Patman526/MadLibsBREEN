07f48d73881d0d188c78638c67b60b450fb035a2
Patrick <breenboyz@gmail.com>
2019-09-02 19:43:07 -0500

    Done pretty much

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         EditText editNoun = findViewById(R.id.editNoun);
         EditText editVerb = findViewById(R.id.editVerb);
         EditText editName = findViewById(R.id.editName);
+        EditText editVerb2 = findViewById(R.id.editVerb2);
 
         String adjectiveStr = editAdjective.getText().toString();
         String nounStr = editNoun.getText().toString();
         String verbStr = editVerb.getText().toString();
         String nameStr = editName.getText().toString();
+        String verb2Str = editVerb2.getText().toString();
+
+        if(adjectiveStr.equals("") || nounStr.equals("") || verbStr.equals("") || nameStr.equals("") || verb2Str.equals("")){
+            Toast toast = Toast.makeText(getApplicationContext(), "Please fill out all fields", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
 
         Intent intent = new Intent(this, TheLib.class);
 
@@ -31,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(TheLib.NOUN, nounStr);
         intent.putExtra(TheLib.VERB, verbStr);
         intent.putExtra(TheLib.NAME, nameStr);
+        intent.putExtra(TheLib.VERB2, verb2Str);
 
         startActivity(intent);
     }
--- app/src/main/java/com/example/madlibs/TheLib.java
+++ app/src/main/java/com/example/madlibs/TheLib.java
@@ -3,14 +3,17 @@ package com.example.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TheLib extends AppCompatActivity {
 
-    public static final String ADJECTIVE = "adjective";
+    public static final String ADJECTIVE = "Adjective";
     public static final String NOUN = "Noun";
-    public static final String VERB = "verb";
+    public static final String VERB = "Verb";
     public static final String NAME = "Name";
+    public static final String VERB2 = "Verb2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,26 @@ public class TheLib extends AppCompatActivity {
         String nounStr = intent.getStringExtra(NOUN);
         String verbStr = intent.getStringExtra(VERB);
         String nameStr = intent.getStringExtra(NAME);
+        String verb2Str = intent.getStringExtra(VERB2);
 
-        String strToDisplay = "";
-        TextView str = (TextView) findViewById(R.id.info);
+        String strToDisplay = nameStr + " was excited to " + verbStr + " with their " + adjectiveStr + " " + nounStr +
+                ". They were so excited, in fact, that they " + verb2Str;
+        TextView str = (TextView) findViewById(R.id.finalLib);
         str.setText(strToDisplay);
+    }
+
+    public void shareInfo(View v){
+        TextView textLib = findViewById(R.id.finalLib);
+
+        String libStr = textLib.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, libStr);
+
+        String chooserTitle = "Select an app to share to";
+
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
     }
 }
